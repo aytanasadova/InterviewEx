eea61046875343cd78205f7eeddb841ce120e744
Aytan_Asadova <aytanasadova1993@gmail.com>
2022-09-12 12:40:38 +0200

    Staircase

---
+++ src/myExercises/hackerrank/general/Algorithms/staircase/Staircase.java
@@ -0,0 +1,32 @@
+package myExercises.hackerrank.general.Algorithms.staircase;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Staircase {
+
+    public static void staircase(int n) {
+        // Write your code here
+        for (int m = 1; m <= n; m++) {
+
+            for (int i = n - m; i > 0; i--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= m; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        staircase(n);
+
+        bufferedReader.close();
+    }
+}
