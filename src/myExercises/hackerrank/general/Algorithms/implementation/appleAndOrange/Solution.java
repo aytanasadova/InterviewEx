0806dcef7c2c67be8f69332cc1be513c7c810bb2
Aytan_Asadova <aytanasadova1993@gmail.com>
2022-09-14 22:22:07 +0200

    apple and orange

---
+++ src/myExercises/hackerrank/general/Algorithms/implementation/appleAndOrange/Solution.java
@@ -0,0 +1,75 @@
+package myExercises.hackerrank.general.Algorithms.implementation.appleAndOrange;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+class Result {
+
+    /*
+     * Complete the 'countApplesAndOranges' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER s
+     *  2. INTEGER t
+     *  3. INTEGER a
+     *  4. INTEGER b
+     *  5. INTEGER_ARRAY apples
+     *  6. INTEGER_ARRAY oranges
+     */
+
+    public static void countApplesAndOranges(int s, int t, int a, int b, List<Integer> apples,     List<Integer> oranges) {
+        // Write your code here
+        int i,appleCount=0,orangeCount=0,aUnit, oUnit;
+        for(i=0;i<apples.size();i++){
+            aUnit=apples.get(i);
+            if ( aUnit>0 && aUnit+a>=s && aUnit+a<=t) appleCount++;
+        }
+        for (i=0;i<oranges.size();i++){
+            oUnit=oranges.get(i);
+            if (oUnit<0 && b+oUnit<=t && b+oUnit>=s) orangeCount++;
+        }
+        System.out.println(appleCount);
+        System.out.println(orangeCount);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int s = Integer.parseInt(firstMultipleInput[0]);
+
+        int t = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int a = Integer.parseInt(secondMultipleInput[0]);
+
+        int b = Integer.parseInt(secondMultipleInput[1]);
+
+        String[] thirdMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int m = Integer.parseInt(thirdMultipleInput[0]);
+
+        int n = Integer.parseInt(thirdMultipleInput[1]);
+
+        List<Integer> apples = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> oranges = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        Result.countApplesAndOranges(s, t, a, b, apples, oranges);
+
+        bufferedReader.close();
+    }
+}
